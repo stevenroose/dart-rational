ed4c77d2d79b7405efc70baf5aabe0792d7cea43
Steven Roose <stevenroose@gmail.com>
2014-10-29 21:25:45 +0100

    Fixed bug in Rational.precision

--- lib/rational.dart
+++ lib/rational.dart
@@ -307,7 +307,7 @@ abstract class Rational<T extends dynamic/*int|BigInt*/> implements Comparable<R
       x = x * _INT_10;
     }
     x = x ~/ _denominator;
-    return x.toString().length;
+    return x.abs().toString().length;
   }
 
   /**
